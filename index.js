b38d53286b7a185f078767c6ce0f0cb1e59fb9c3
Hao Wang <wang.hao.chnbj@gmail.com>
2024-01-04 16:40:23 -0500

    Add html file to test service.

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
 const express = require('express');
 const app = express();
+const fileUpload = require('express-fileupload');
+const {Storage} = require('@google-cloud/storage');
+
+
+app.use(
+  fileUpload({
+      limits: {
+          fileSize: 10000000,
+      },
+      abortOnLimit: true,
+  })
+);
+
+// Add this line to serve our index.html page
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
+  res.send('Hello World!');
+});
+
+app.put('/image', async (req, res) => {
+  // Get the file that was set to our field named "image"
+  const { image } = req.files;
+
+  // If no image submitted, exit
+  if (!image) {
+    console.log('no image selected!');
+    return res.sendStatus(400);
+  }
+
+  console.log(req.files);
+  // image.mv('uploads/' + image.name);
+
+  try {
+    const storage = new Storage();
+    const bucketName = 'deepstream-experiments-comfyui'
+    const bucket = storage.bucket(bucketName)
+    const blob = bucket.file(image.name); // Preserve original filename
+    await blob.save(image.data);
+    console.log('Image uploaded successfully to GCS:', blob.publicUrl());
+  } catch (error) {
+    console.error('Error uploading image:', error);
+    res.status(500).send('Error uploading image');
+  }
+
+  // All good
+  res.sendStatus(200);
 });
 
 const port = parseInt(process.env.PORT) || 8080;
